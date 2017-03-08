4ed138e7145c64175e2420eec19cd97c8a02bb6d
kellyh6 <faithkeeping@yahoo.com>
2017-03-08 13:57:54 -0800

    updated middleware

---
+++ middleware/islogin.js
@@ -0,0 +1,10 @@
+module.exports = function(req,res,next){
+	if(!req.user){
+		req.flash('error', 'You must me logged in to view this page');
+		res.redirect('/auth/login');
+	}
+	else{
+		next();
+	}
+}
+
